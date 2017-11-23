469690adbc17fb2f28a40240f04654c6121ddc0b
Bielo <bielo7@o2.pl>
2017-11-23 21:00:00 +0100

    poczatek

--- src/main/java/pl/sdacademy/Employee.java
+++ src/main/java/pl/sdacademy/Employee.java
@@ -4,10 +4,17 @@ package pl.sdacademy;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 public abstract class Employee {
     private String name;
     private String adress;
     private String bankAccountNumber;
+
+    public abstract boolean isPaymentDay(LocalDate day);
+
+    public abstract BigDecimal calculatePayment(LocalDate day);
 }
--- src/main/java/pl/sdacademy/HourlyEmployee.java
+++ src/main/java/pl/sdacademy/HourlyEmployee.java
@@ -1,7 +1,20 @@
 package pl.sdacademy;
 
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
 public class HourlyEmployee extends Employee {
-    public HourlyEmployee(String name, String adress, String bankAccountNumber) {
+
+    private static final int REGULAR_HOURS = 8;
+    private static final BigDecimal OVERTIME_RATE = new BigDecimal("1.5");
+
+    private BigDecimal hourlyRate;
+    private List<WorkingDay> workingDays;
+
+    public HourlyEmployee(String name, String adress, String bankAccountNumber, BigDecimal hourlyRate) {
         super(name, adress, bankAccountNumber);
+        this.hourlyRate = hourlyRate;
+        workingDays = new LinkedList<>();
     }
 }
